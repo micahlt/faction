e6595fdd59e93e93fa3a72b6629293041ef78fb8
CC <CarolineMcCoy3@gmail.com>
2026-04-20 16:33:52 -0500

    Issue 27
    Add Browser notifications for inactive tab

--- apps/web/src/routes/__root.jsx
+++ apps/web/src/routes/__root.jsx
@@ -1,11 +1,41 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { useEffect } from "react";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  useEffect(() =>{
+    // Asks user for notification permissions 
+    if("Notification" in window){
+      if(Notification.permission == "default"){
+        Notification.requestPermission();
+      }
+    };
+
+    // Enables display of notification
+    const showNot = (title, body) => {
+      if(!("Notification" in window)) return;
+      if(Notification.permission == "granted"){
+        new Notification(title, {body});
+      }
+    };
+
+    // Allows for visibility changes
+    const visChange = () => {
+      if(document.visibilityState === "hidden"){
+        showNot(
+          "Inactive Tab", "Something Happened"
+        );
+      }
+    };
+    document.addEventListener("visibilitychange", visChange);
+    return () => {
+      document.removeEventListener("visibilitychange", visChange);
+    };
+  }, []);
   return (
     <>
       <Outlet />
