ef97495ea629d179d7953822b065afc53a41c966
Micah Lindley <hi@micahlindley.com>
2026-04-28 23:39:38 -0500

    Move image lightbox to a React portal

--- apps/web/src/components/Message.jsx
+++ apps/web/src/components/Message.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import UserAvatar from "./UserAvatar";
 import s from "../styles/modules/Message.module.css";
 import classNames from "classnames";
@@ -11,44 +12,48 @@ export default function Message({ message = {}, hideAuthor = false }) {
   const imageMessage = isImageUrl(message.content);
 
   return (
-    <div className={classNames(s.message, hideAuthor ? s.hiddenAuthor : "")}>
-      {hideAuthor ? <div className={s.avatarPlaceholder} /> : <UserAvatar imageUrl={message?.author?.imageUrl} />}
+    <>
+      <div className={classNames(s.message, hideAuthor ? s.hiddenAuthor : "")}>
+        {hideAuthor ? <div className={s.avatarPlaceholder} /> : <UserAvatar imageUrl={message?.author?.imageUrl} />}
 
-      <div className={s.main}>
-        {!hideAuthor && (
-          <span className={s.metadata}>
-            <p>{message.author.nickname}</p>
-            <p className={s.date}>
-              {new Date(message.createdAt).toLocaleString("en-US", {
-                hour12: true,
-                minute: "numeric",
-                hour: "numeric",
-              })}
-            </p>
-          </span>
-        )}
-
-        <div className={s.content}>
-          {imageMessage ? (
-            <img
-              src={message.content}
-              alt="Sent attachment"
-              className={s.messageImage}
-              onClick={() => setExpanded(true)}
-            />
-          ) : (
-            <LinkItUrl className={s.contentLink}>
-              {message.content}
-            </LinkItUrl>
+        <div className={s.main}>
+          {!hideAuthor && (
+            <span className={s.metadata}>
+              <p>{message.author.nickname}</p>
+              <p className={s.date}>
+                {new Date(message.createdAt).toLocaleString("en-US", {
+                  hour12: true,
+                  minute: "numeric",
+                  hour: "numeric",
+                })}
+              </p>
+            </span>
           )}
+
+          <div className={s.content}>
+            {imageMessage ? (
+              <img
+                src={message.content}
+                alt="Sent attachment"
+                className={s.messageImage}
+                onClick={() => setExpanded(true)}
+              />
+            ) : (
+              <LinkItUrl className={s.contentLink}>
+                {message.content}
+              </LinkItUrl>
+            )}
+          </div>
         </div>
       </div>
 
-      {expanded && (
-        <div className={s.lightbox} onClick={() => setExpanded(false)}>
-          <img src={message.content} alt="Expanded attachment" />
-        </div>
-      )}
-    </div>
+      {expanded &&
+        createPortal(
+          <div className={s.lightbox} onClick={() => setExpanded(false)}>
+            <img src={message.content} alt="Expanded attachment" />
+          </div>,
+          document.getElementById("portal-root")
+        )}
+    </>
   );
 }
