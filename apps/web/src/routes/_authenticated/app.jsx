5fa8a4180d2fea51d8cb9880ad5c9de4f0f119bf
Micah Lindley <hi@micahlindley.com>
2026-04-28 20:28:59 -0500

    Move notification permission request to app page

--- apps/web/src/routes/_authenticated/app.jsx
+++ apps/web/src/routes/_authenticated/app.jsx
@@ -35,6 +35,31 @@ function RouteComponent() {
     return () => clearInterval(int);
   }, [socket]);
 
+  useEffect(() => {
+    if ("Notification" in window) {
+      if (Notification.permission == "default") {
+        Notification.requestPermission();
+      }
+    }
+
+    const showNot = (title, body) => {
+      if (!("Notification" in window)) return;
+      if (Notification.permission == "granted") {
+        new Notification(title, { body });
+      }
+    };
+
+    const visChange = () => {
+      if (document.visibilityState === "hidden") {
+        showNot("Inactive Tab", "Something Happened");
+      }
+    };
+    document.addEventListener("visibilitychange", visChange);
+    return () => {
+      document.removeEventListener("visibilitychange", visChange);
+    };
+  }, []);
+
   if (!userLoading && firstFactionId && isAppIndexPath) {
     return (
       <Navigate to="/app/$factionId" from="/" params={{ factionId: firstFactionId }} replace />
