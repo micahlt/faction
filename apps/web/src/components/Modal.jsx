f616b4cc576c9be4285023e44dadef80339116e4
Micah Lindley <hi@micahlindley.com>
2026-04-30 17:42:58 -0500

    Reworks to modals

--- apps/web/src/components/Modal.jsx
+++ apps/web/src/components/Modal.jsx
@@ -1,11 +1,14 @@
 import s from "../styles/modules/Modal.module.css";
+import { XIcon } from "@phosphor-icons/react";
 
-export default function Modal({ canCloseOnOverlay = true, onClose = () => {}, children }) {
+export default function Modal({ canCloseOnOverlay = true, onClose = () => { }, children }) {
   return (
     <div className={s.modalParent}>
       <div className={s.overlay} onClick={onClose} />
-      <div className={s.content}>
-        <div className={s.contentWrapper}>{children}</div>
+      <div className={s.content}>{children}
+        {canCloseOnOverlay && <button className={s.closeButton} onClick={onClose}>
+          <XIcon size={24} />
+        </button>}
       </div>
     </div>
   );
